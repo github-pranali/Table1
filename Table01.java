bd38c6927f8d72d6276efe08e2b68f05f4f20dc0
github-pranali <pranali2905patil@gmail.com>
2024-08-26 19:32:38 +0530

    Add files via upload

---
+++ Table01.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.*;
+
+public class Table01
+{
+    public static void main(String[] args)
+    {
+        int No =0, i=0;
+
+        Scanner S = new Scanner(System.in);
+
+        System.out.println("Enter a Number : ");
+        No =S.nextInt();
+
+        for(i = 1 ; i <=10; i++)
+        {
+            System.out.println(" " + No + " * " + i + " = " + No * i);
+        }
+    }
+}
